d56dea0256178d7de6b95d25d687f82ddd18fb83
Anna Kipilová <anna.kipilova@student.tuke.sk>
2023-05-07 23:29:50 +0200

    init

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ require('dotenv').config();
 app.use(cors()); // it has to be there always
 app.use(express.json()); // this too
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 let db;
 
