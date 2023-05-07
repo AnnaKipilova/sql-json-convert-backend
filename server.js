687d0a301b0031e0c9c1e416417ae372ccee6ebb
Anna Kipilová <anna.kipilova@student.tuke.sk>
2023-05-08 00:05:56 +0200

    init

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(cors({
 }));
 app.use(express.json()); // this too
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sql-to-json-converter.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
